deab1d8e6dfe3c005749568bbb67f85407a9dc1b
Clinton Bentley <clinton.bentley@gmail.com>
2017-06-19 23:31:20 +0200

    visual changes

--- web/public/js/light-channels.js
+++ web/public/js/light-channels.js
@@ -1,6 +1,11 @@
 var chan_number = 6;
 // set the dimensions and margins of the graph
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 50
+    },
     width = 800 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -14,8 +19,7 @@ var svg = d3.select("#channel_chart1").append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")")
-    ;
+        "translate(" + margin.left + "," + margin.top + ")");
 
 var ourdata = new Array();
 var channeldata = new Array();
@@ -41,13 +45,29 @@ d3.json("/channelprog/" + profile, function (error, json) {
     if (error) return console.warn(error);
     console.log("got channel program data for profile " + profile);
     json.forEach(function (d) {
-        if (!ourdata[d.channel]) { ourdata[d.channel] = { data: [{ time: parseTime(d.time), power: +d.power, channel: +d.channel }] } }
-        else { ourdata[d.channel].data.push({ time: parseTime(d.time), power: +d.power, channel: +d.channel }); }
+        if (!ourdata[d.channel]) {
+            ourdata[d.channel] = {
+                data: [{
+                    time: parseTime(d.time),
+                    power: +d.power,
+                    channel: +d.channel
+                }]
+            }
+        } else {
+            ourdata[d.channel].data.push({
+                time: parseTime(d.time),
+                power: +d.power,
+                channel: +d.channel
+            });
+        }
     });
     d3.json("/channels/", function (error, json) {
         if (error) return console.warn(error);
         json.forEach(function (d) {
-            channeldata[d.id] = { name: d.name, class: d.class };
+            channeldata[d.id] = {
+                name: d.name,
+                class: d.class
+            };
         });
 
         build_chart(ourdata);
@@ -74,25 +94,26 @@ function build_chart(data) {
         .style("text-anchor", "end")
         .attr("dx", "-.8em")
         .attr("dy", ".15em")
-        .attr("transform", "rotate(-25)")
-        ;
+        .attr("transform", "rotate(-25)");
 
     // add the Y Axis
     svg.append("g")
         .call(d3.axisLeft(scaley));
 
     valueline = d3.line()
-        .x(function (d) { return scalex(d.time); })
-        .y(function (d) { return scaley(d.power); })
-        .curve(d3.curveMonotoneX)
-        ;
+        .x(function (d) {
+            return scalex(d.time);
+        })
+        .y(function (d) {
+            return scaley(d.power);
+        })
+        .curve(d3.curveMonotoneX);
 
     for (i = 0; i < chan_number; i++) {
         svg.append("path")
             .data([data[i].data])
             .attr("class", "channel" + i)
-            .attr("d", valueline).attr("id", "channel" + i)
-            ;
+            .attr("d", valueline).attr("id", "channel" + i);
         updatepath(i);
         updatedot(i);
     }
@@ -108,16 +129,26 @@ function dragged(d) {
     var nx = d3.event.x;
     var ny = d3.event.y;
 
-    if (scaley.invert(ny) < 0) { ny = scaley(0) };
-    if (scaley.invert(ny) > 100) { ny = scaley(100) };
-    if (scalex.invert(nx) < parseTime("00:00:00")) { nx = scalex(parseTime("00:00:00")) };
-    if (scalex.invert(nx) > parseTime("24:00.00")) { nx = scalex(parseTime("24:00.00")) };
+    if (scaley.invert(ny) < 0) {
+        ny = scaley(0)
+    };
+    if (scaley.invert(ny) > 100) {
+        ny = scaley(100)
+    };
+    if (scalex.invert(nx) < parseTime("00:00:00")) {
+        nx = scalex(parseTime("00:00:00"))
+    };
+    if (scalex.invert(nx) > parseTime("24:00.00")) {
+        nx = scalex(parseTime("24:00.00"))
+    };
 
     d3.select(this).attr("cx", d.x = nx).attr("cy", d.y = ny);
     d.power = scaley.invert(ny);
     d.time = scalex.invert(nx);
 
-    ourdata[d.channel].data.sort(function (a, b) { return (a.time < b.time) });
+    ourdata[d.channel].data.sort(function (a, b) {
+        return (a.time < b.time)
+    });
     updatepath(d.channel);
     updateform(d.channel);
 }
@@ -129,23 +160,35 @@ function dragended(d) {
 function updatepath(i) {
     svg.selectAll("#channel" + i).attr("d", valueline);
 }
+
 function updatedot(i) {
 
-    if (svg.select("#channeldots" + i)) { console.log("new " + i); svg.append("g").attr("id", "channeldots" + i) }
+    if (svg.select("#channeldots" + i)) {
+        console.log("new " + i);
+        svg.append("g").attr("id", "channeldots" + i)
+    }
     svg.select("#channeldots" + i).selectAll("circle")
         .data(ourdata[i].data)
         .enter().append("circle")
         .attr("r", 4)
-        .attr("cx", function (d) { return scalex(d.time); })
-        .attr("cy", function (d) { return scaley(d.power); })
+        .attr("cx", function (d) {
+            return scalex(d.time);
+        })
+        .attr("cy", function (d) {
+            return scaley(d.power);
+        })
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended));
 
     svg.select("channeldots" + i).selectAll("circle").data(ourdata[i].data).transition().duration(100).attr("r", 6)
-        .attr("cx", function (d) { return scalex(d.time); })
-        .attr("cy", function (d) { return scaley(d.power); });
+        .attr("cx", function (d) {
+            return scalex(d.time);
+        })
+        .attr("cy", function (d) {
+            return scaley(d.power);
+        });
 
     svg.select("channeldots" + i).selectAll("circle").data(ourdata[i].data).exit().remove();
 
@@ -153,9 +196,9 @@ function updatedot(i) {
 }
 
 function updateform(i) {
-    d3.select("#pointlist"+i).selectAll("li").data(ourdata[i].data).enter().append("li").text(pretty_point);
-    d3.select("#pointlist"+i).selectAll("li").data(ourdata[i].data).transition().text(pretty_point)
-    d3.select("#pointlist"+i).selectAll("li").data(ourdata[i].data).exit().remove();
+    d3.select("#pointlist" + i).selectAll("li").data(ourdata[i].data).enter().append("li").text(pretty_point);
+    d3.select("#pointlist" + i).selectAll("li").data(ourdata[i].data).transition().text(pretty_point)
+    d3.select("#pointlist" + i).selectAll("li").data(ourdata[i].data).exit().remove();
 }
 
 function makechannelcontrolui() {
@@ -169,21 +212,21 @@ function makechannelcontrolui() {
         var contents = t.append("div").attr("id", "collapse" + i).attr("class", "panel-collapse collapse").attr("role", "tabpanel").attr("aria-labelledby", "heading" + i)
             .append("div").attr("class", "panel-body").append("form").attr("class", "form-inline");
         var t = contents.append("div").attr("class", "form-group");
-        t.append("label").attr("for", "EditName"+i).text("Name");
-        t.append("input").attr("type", "text").attr("class", "form-control").attr("id", "EditName"+i).attr("value", channeldata[i].name);
+        t.append("label").attr("for", "EditName" + i).text("Name");
+        t.append("input").attr("type", "text").attr("class", "form-control").attr("id", "EditName" + i).attr("value", channeldata[i].name);
         var t = contents.append("div").attr("class", "form-group");
-        t.append("label").attr("for", "EditClass"+i).text("Class");
-        t.append("input").attr("type", "text").attr("class", "form-control").attr("id", "EditClass"+i).attr("value", channeldata[i].class);
+        t.append("label").attr("for", "EditClass" + i).text("Class");
+        t.append("input").attr("type", "text").attr("class", "form-control").attr("id", "EditClass" + i).attr("value", channeldata[i].class);
         var t = contents.append("div").attr("class", "form-group");
         t.append("button").attr("type", "submit").attr("class", "btn btn-default").text("Update");
-        var ul=contents.append("ul").attr("id","pointlist"+i);
-        
-        updateform(i);    
-        
+        var ul = contents.append("ul").attr("id", "pointlist" + i);
+
+        updateform(i);
+
     }
 
 }
 
 function pretty_point(point) {
-    return "Time: "+d3.timeFormat("%H:%M")(point.time)+" Power %: "+Math.round(point.power);
+    return "Time: " + d3.timeFormat("%H:%M")(point.time) + " Power %: " + Math.round(point.power);
 }
